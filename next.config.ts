43a66cf3b5ab24025d558b194da4326f03c074a6
Anisul Islam <anis.inbox10@gmail.com>
2026-05-19 21:19:58 +0500

    feat: add HeroSection component, configure image remote patterns, and include Google site verification files

--- next.config.ts
+++ next.config.ts
@@ -9,100 +9,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "alwaqaspaint.com",
+        hostname: "**",
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: "http",
+        hostname: "**",
       },
-      {
-        protocol: "https",
-        hostname: "backiee.com",
-      },
-      {
-        protocol: "https",
-        hostname: "nelsonpaints.com",
-      },
-      {
-        protocol: "https",
-        hostname: "img.magnific.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-      },
-      {
-        protocol: "https",
-        hostname: "thestationers.pk",
-      },
-      {
-        protocol: "https",
-        hostname: "gobispaints.com",
-      },
-      {
-        protocol: "https",
-        hostname: "sparcopaints.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.dulux.com.pk",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "ideahobby.eu",
-      },
-      {
-        protocol: "https",
-        hostname: "media.fortuneindia.com",
-      },
-      {
-        protocol: "https",
-        hostname: "nipponpaint.com.pk",
-      },
-      {
-        protocol: "https",
-        hostname: "faisalsanitary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "porta.pk",
-      },
-      {
-        protocol: "https",
-        hostname: "iysqidwpmfcierwdnxsp.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "supertuff.pk",
-      },
-      {
-        protocol: "https",
-        hostname: "fittings.com.pk",
-      },
-      {
-        protocol: "https",
-        hostname: "lamasatzim.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.smartmaterials.pk",
-      },
-      {
-        protocol: "https",
-        hostname: "d3genk2jhzijgn.cloudfront.net",
-      },
-      {
-        protocol: "https",
-        hostname: "i0.wp.com",
-      }
     ],
   },
 
